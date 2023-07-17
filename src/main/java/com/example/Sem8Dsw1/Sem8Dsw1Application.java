6cccad84938fab228f6d4490c6d1a4055e507094
DarkCoder1323 <rayvargas1323@gmail.com>
2023-07-17 07:40:21 -0500

    Update

---
+++ src/main/java/com/example/Sem8Dsw1/Sem8Dsw1Application.java
@@ -0,0 +1,13 @@
+package com.example.Sem8Dsw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem8Dsw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem8Dsw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sem8Dsw1/Sem8Dsw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sem8Dsw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem8Dsw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
